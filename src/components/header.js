4b84344acec3025bc5c91a99c809e1d9ba1f5621
Cale Sennett <calesennett@gmail.com>
2020-11-14 11:12:09 -0800

    initial commit

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,27 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
+import logo from '../images/drive_ping.png';
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+import {
+  Grid,
+  Box,
+  Image,
+  Container
+} from 'theme-ui';
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
+function Header() {
+  return (
+    <Container>
+      <Grid>
+        <Image width='100' src={logo} />
+        <Box
+          sx={{
+            justifySelf: 'end'
+          }}>
+          Message now
+        </Box>
+      </Grid>
+    </Container>
+  )
 }
 
 export default Header
---
+++ src/components/theme.js
@@ -0,0 +1,12 @@
+export default {
+  colors: {
+    background: 'white'
+  },
+  fonts: {
+    body: 'Sohne, sans-serif',
+    heading: 'Sohne, sans-serif'
+  },
+  sizes: {
+    container: 960
+  }
+}
